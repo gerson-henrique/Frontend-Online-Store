934444c15a6e7447ef69207deac5576211e188ad
Gero <gersonhoa@gmail.com>
2022-01-21 17:48:19 -0300

    [Grupo 10] - Requisito 8

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Paginas/Home';
 import CarrinhoDeCompras from './Paginas/CarrinhoDeCompras';
@@ -9,15 +9,7 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <div id="cartButtonContainer">
-          <Link
-            data-testid="shopping-cart-button"
-            to="/carrinho"
-            id="cartButton"
-          >
-            Carrinho
-          </Link>
-        </div>
+        <div id="cartButtonContainer" />
         <Route exact path="/" component={ Home } />
         <Route path="/carrinho" component={ CarrinhoDeCompras } />
         <Route path="/item/:id" render={ (props) => (<ProductDetail { ...props } />) } />
