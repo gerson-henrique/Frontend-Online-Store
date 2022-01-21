8b7da61aa906af9cbaae32f5099bd7875d97d069
george lucas <glalmeida.dev@gmail.com>
2022-01-21 19:24:54 -0300

    [Grupo 10 - requisito 9]

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
 import Home from './Paginas/Home';
 import CarrinhoDeCompras from './Paginas/CarrinhoDeCompras';
@@ -8,8 +8,17 @@ import ProductDetail from './Paginas/ProductDetail';
 class App extends React.Component {
   render() {
     return (
+
       <BrowserRouter>
-        <div id="cartButtonContainer" />
+        <div id="cartButtonContainer">
+          <Link
+            data-testid="shopping-cart-button"
+            to="/carrinho"
+            id="cartButton"
+          >
+            Carrinho
+          </Link>
+        </div>
         <Route exact path="/" component={ Home } />
         <Route path="/carrinho" component={ CarrinhoDeCompras } />
         <Route path="/item/:id" render={ (props) => (<ProductDetail { ...props } />) } />
